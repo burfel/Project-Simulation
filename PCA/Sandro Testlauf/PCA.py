0b94293dcea9dfc63eb9affbf8d31250765efd7c
Sawrz <sandro.wrzalek@fu-berlin.de>
2015-11-01 16:50:10 +0100

    PCA Update
    Changelog:
    - renaming of some functions and variables
    - datasetGenerator is now a function
    - PCA with SVD is no added (don't get same transformationresults like
    with covMat)
    - added some more debugOptions

--- PCA/Sandro Testlauf/PCA.py
+++ PCA/Sandro Testlauf/PCA.py
@@ -1,77 +1,72 @@
 ﻿import numpy as np
+import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
 
 
 
-
-# GENERAL SETTINGS
-generate_datasets = True
-
-
 # GENERATE SOME DATASETS
 
-## DEBUG SETTINGS
-print_debug_datasets = False
-plot_debug_datasets = False 
+def generateDataset (measurementDimension, numberOfDatapoints, valueMu = 0, generateRandomMu = False, generateRandomCovMatrix = False, symmetricMatrix = True):
 
-if generate_datasets :
+    # DEBUG SETTINGS
+    print_debug_dataset = False
+    plot_debug_dataset = False 
 
-    # SETTINGS
-    dimension = 3
-    number_of_datapoints = 5
-    generate_random_covMatrix = False 
-    generate_random_mu = False
-    mu_value = 0
 
     # GENERATE
-    mu_generate = np.zeros( (dimension) )
-    cov_mat_generate = np.zeros ( ( dimension , dimension ) )
+    mu_generate = np.zeros( (measurementDimension) )
+    cov_mat_generate = np.zeros ( ( measurementDimension , measurementDimension ) )
 
-    if generate_random_covMatrix:
-        symm_counter = 1
-        for i in range (dimension):
-            for j in range (symm_counter):
-                cov_mat_generate[i,j] = np.random.randint(10)
-                cov_mat_generate[j,i] = cov_mat_generate[i,j]
-            symm_counter = symm_counter + 1
+    if generateRandomCovMatrix:
+        if symmetricMatrix:
+            symm_counter = 1
+            for i in range (measurementDimension):
+                for j in range (symm_counter):
+                    cov_mat_generate[i,j] = np.random.randint(10)
+                    cov_mat_generate[j,i] = cov_mat_generate[i,j]
+                symm_counter = symm_counter + 1
+        else: 
+            for i in range (measurementDimension):
+                for j in range (measurementDimension):
+                    cov_mat_generate[i,j] = np.random.randint(10)
      
 
     else:
-        for i in range (dimension):
+        for i in range (measurementDimension):
             cov_mat_generate[i,i] = 1
 
 
-    if generate_random_mu:
-        for i in range (dimension):
+    if generateRandomMu:
+        for i in range (measurementDimension):
             mu_generate[i] = np.random.randint(10)
 
     else:
-        if mu_value != 0:
-            for i in range (dimension):
-                mu_generate[i] = mu_value
+        if valueMu != 0:
+            for i in range (measurementDimension):
+                mu_generate[i] = valueMu
     
 
-    datasets = np.random.multivariate_normal(mu_generate, cov_mat_generate, number_of_datapoints).T
+    dataset = np.random.multivariate_normal(mu_generate, cov_mat_generate, numberOfDatapoints).T
 
     # DEBUG
-    if print_debug_datasets:
+    if print_debug_dataset:
         print "Covariance Matrix: \n" , cov_mat_generate 
         print "mu: \n" , mu_generate
-        print "Dataset: \n" , datasets , "\n"
-        print "Shape dataset: " + str(datasets.shape)
+        print "Dataset: \n" , dataset , "\n"
+        print "Shape dataset: " + str(dataset.shape)
 
-    if plot_debug_datasets:
-        if dimension == 2:
-            plt.plot (datasets [0,:], datasets [1,:], "ro")
+    if plot_debug_dataset:
+        if measurementDimension == 2:
+            plt.plot (dataset [0,:], dataset [1,:], "ro")
             plt.show ()
 
-        if dimension == 3:
+        if measurementDimension == 3:
             fig = plt.figure(figsize=(8,8))
             ax = fig.add_subplot(111, projection='3d')
             plt.rcParams['legend.fontsize'] = 10
-            ax.plot(datasets [0,:], datasets [1,:], datasets [2,:], 'o', markersize=8, color='blue', alpha=0.5, label='Dataset 1')
+            ax.plot(dataset [0,:], dataset [1,:], dataset [2,:], 'o', markersize=8, color='blue', alpha=0.5, label='Dataset 1')
             
             plt.title('Samples for Dataset 1')
             ax.legend(loc='upper right')
@@ -79,6 +74,9 @@ if generate_datasets :
 
         else:
             print "Can't Plot! Check your Dimensions!"
+
+    # RETURN
+    return dataset
 
 
 
@@ -107,7 +105,7 @@ def getCovMat (dataset):
     debug_covMat = False
 
     # GENERAL VARIABLES
-    n = datasets.shape[1] - 1 
+    n = dataset.shape[1] - 1 
     
     # DEBUG
     if debug_covMat:
@@ -123,12 +121,11 @@ def getCovMat (dataset):
 
     return covMat
 
-############################################################################
 
-def mean_free_data (dataset, inputDimension):
+def meanFreeData (dataset, inputDimension):
 
     # DEBUG SETTINGS
-    debug_mean_free_data = False
+    debug_meanFreeData = False
 
     # GET DATA
     meanVector = getMean (dataset , inputDimension)
@@ -143,24 +140,24 @@ def mean_free_data (dataset, inputDimension):
             meanFreeDataset[i, j] = dataset[i, j] - meanVector[i]
 
     # DEBUG
-    if debug_mean_free_data:
+    if debug_meanFreeData:
         print "original dataset: \n" , dataset
         print "mean free dataset :\n" , meanFreeDataset
 
     return meanFreeDataset
 
-##########################################################################
 
-def PCA_with_COV (dataset, inputDimension , outputDimension, meanFreeData = True):
+def pcaWithCovMat (dataset, inputDimension , outputDimension, meanFreeDatapoints = True):
 
     #DEBUG SETTINGS
     testing_values = True
     debug_PCA = False
+    show_transformation_matrix = False
 
     #GET DATA AND EIGENVECTORS
 
-    if meanFreeData:
-        meanFreeDataset = mean_free_data (dataset, inputDimension)
+    if meanFreeDatapoints:
+        meanFreeDataset = meanFreeData (dataset, inputDimension)
         covMat = getCovMat (meanFreeDataset)
 
     else:
@@ -207,12 +204,12 @@ def PCA_with_COV (dataset, inputDimension , outputDimension, meanFreeData = True
         for j in range ( inputDimension ):
                 transMat[i,j] = eig_pairs[i][1][j]
 
-    if debug_PCA:
+    if debug_PCA or show_transformation_matrix:
         print "Transformationmatrix: \n" , transMat , "\n"
 
     # TRANSFORM DATASET
 
-    if meanFreeData:
+    if meanFreeDatapoints:
         transformed_dataset = transMat.dot( meanFreeDataset )
 
     else:
@@ -230,7 +227,90 @@ def PCA_with_COV (dataset, inputDimension , outputDimension, meanFreeData = True
     return transformed_dataset
 
 
+###################################################################################################
+
+
+def pcaWithSVD (dataset, inputDimension , outputDimension):
+
+    # DEBUG SETTINGS
+    debug_PCA = False
+    show_transformation_matrix = False
+
+    # GET MEANFREE DATA
+
+    meanFreeDataset = meanFreeData (dataset, inputDimension)
+
+    # CONSTRUCT the Y-MATRIX
+    
+    n = dataset.shape[1] - 1
+
+    if debug_PCA:
+        print "n: " , n , "\n"
+    
+    newDatasetMatrix = 1. / math.sqrt ( n ) * dataset.T
+
+    # DO SVD
+    measurementDimensionMatrix, singularValues, principleComponents = np.linalg.svd(newDatasetMatrix)
+
+    if debug_PCA:
+        print "Measurement Dimension Matrix: \n" , "Shape: " , measurementDimensionMatrix.shape , "\n" , measurementDimensionMatrix , "\n"
+        print "singularValues: \n" , "Shape: " , singularValues.shape , "\n" , singularValues , "\n"
+        print "Principal Components: \n" , "Shape: " , principleComponents.shape , "\n" , principleComponents , "\n"
+
+    #REDUCE THE NUMBER OF PRINCIPLE COMPONENTS TO THE WANTED DIMENSIONS
+    principleComponentsMatrix = np.zeros ( (inputDimension , outputDimension) )
+
+    for i in range ( inputDimension ):
+        for j in range ( outputDimension ):
+            principleComponentsMatrix[i , j] = principleComponents [i , j]
+
+    if debug_PCA or show_transformation_matrix:
+        print "Principle Components Matrix: \n" , "Shape: " , principleComponentsMatrix.shape , "\n" , principleComponentsMatrix , "\n"
+
+
+    # CALCULATE THE VARIANCES
+
+    varianace = singularValues * singularValues 
+
+    if debug_PCA:
+        print "varianace: \n" , varianace , "\n"
+
+
+    # TRANSFORM THE DATA
+
+    transformed_dataset = principleComponentsMatrix.T.dot(meanFreeDataset)
+
+    if debug_PCA:
+      print "transformed_dataset: \n" , transformed_dataset , "\n"
+
+
+
+    # RETURN
+    return transformed_dataset
   
-  
-print "Dataset tranformed mean free: \n" , PCA_with_COV (datasets, dimension, 2) , "\n"
-print "Dataset tranformed: \n" , PCA_with_COV (datasets, dimension, 2, meanFreeData = False)
+
+###################################################################################################
+
+
+# EVALUATION
+
+## SETTINGS:
+measurementDimension = 3
+outputDimension = 2
+numberOfDatapoints = 5
+
+## ROUTINE
+datasetCov = generateDataset (measurementDimension, numberOfDatapoints , generateRandomCovMatrix = False)
+datasetSVD = np.copy(datasetCov)
+ 
+print "PCA with covariance Matrix: \n"
+print "Dataset tranformed mean free: \n" , pcaWithCovMat (datasetCov, measurementDimension, outputDimension) , "\n"
+# print "Dataset tranformed: \n" , pcaWithCovMat (datasetCov, measurementDimension, outputDimension, meanFreeDatapoints = False)
+
+
+print "\n PCA with SVD: \n"
+print "Dataset transformed: \n" , pcaWithSVD (datasetSVD, measurementDimension , outputDimension)
+
+
+# COMMENTS:
+## covMat and SVD have roughly the same Eigenvalues but the same Eigenvectors of the covMat are not equal to the collums of the PCA of the SVD. Therefore we get also different transformationresults!
