93bfecbdd37d6715817460ac4540d73b8ed9e2b0
Kenneth <k.b@fu-berlin.de>
2015-11-12 23:02:08 +0100

    plot now shows only whole cluster flips

--- Ising/lib/plot.py
+++ Ising/lib/plot.py
@@ -4,17 +4,22 @@ import copy
 import time
 
 class figure:
-    def __init__(self, initialConfig, delta):
+    def __init__(self, initialConfig, delta, flipcount):
         self.initialConfig = initialConfig
         self.delta         = delta
         self.imageList     = [self.initialConfig]
         self.fig           = plt.figure()
+        self.flipcount     = flipcount
     
     def makeImageList(self):
         for i, site in enumerate(self.delta):
             tmp = copy.deepcopy(self.imageList[i])
             tmp[site[0]][site[1]] *= -1
             self.imageList.append(tmp)
+        tmp = copy.deepcopy(self.imageList)
+        self.imageList = []
+        for i in self.flipcount:
+            self.imageList.append(tmp[i])
         return
         
     def init(self):
@@ -30,12 +35,12 @@ class figure:
     
     def animate(self):
         self.makeImageList()
-        self.ani = animation.FuncAnimation(self.fig, self.updatefig, init_func=self.init, frames=len(self.imageList), interval=1, blit=True, repeat_delay=0)
+        self.ani = animation.FuncAnimation(self.fig, self.updatefig, init_func=self.init, frames=len(self.imageList), interval=1, blit=True, repeat_delay=2)
         return self.ani
     
 class show:
-    def __init__(self, initialConfig, delta):
-        self.figure = figure(initialConfig, delta)
+    def __init__(self, initialConfig, delta, flipcount):
+        self.figure = figure(initialConfig, delta, flipcount)
         self.ani = self.figure.animate()
         
     def showPlot(self):
@@ -43,5 +48,5 @@ class show:
 
     def saveVideo(self):
         plt.rcParams['animation.ffmpeg_path'] ='C:\\Program Files\\ffmpeg-20151103-git-6df2c94-win64-static\\bin\\ffmpeg.exe'
-        self.FFwriter = animation.FFMpegWriter(fps=100,bitrate=600)
-        self.ani.save('../output/animation-'+str(int(time.time()))+'.mp4', writer=self.FFwriter, fps=50, extra_args=['-vcodec', 'libx264'])
+        FFwriter = animation.FFMpegWriter(fps=100,bitrate=1600)
+        self.ani.save('output/animation-'+str(int(time.time()))+'.mp4', writer=FFwriter, fps=50, extra_args=['-vcodec', 'libx264'])
