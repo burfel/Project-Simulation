6aeebf48c9151ff667fbd1ac33369fbc09f0229d
janekg89 <janekg89@hotmail.de>
2015-11-19 15:33:26 +0100

    lösche unnützes

--- Cluster/nearestneighbour.py
+++ Cluster/nearestneighbour.py
@@ -6,34 +6,6 @@ class Neighbor(object):
     def __init__(self, d):
         self.d = d
 
-    def nearestneigh(self, p, eps):
-        """
-        gibt die Nachbarn einer Punktes P in der Umgebung eps aus
-        :param p:
-            Der Punkt dessen Nachbarn ermittelt werden sollen
-        :param eps:
-            Ein Punkt ist ein Nachbar wenn er im angegebenen Epsilon-Radius liegt
-        :return: neighbors
-            Die Liste von benachbarten Punkten
-
-        """
-        # sqd ist eine Liste mitEntfernungen hoch2 vom Punkt P zu jedem anderem Punkt in der Liste
-        sqd = []
-        # neighbors ist eine Liste mit den Nachbarn, die innerhalb von eps liegen
-        neighbors = []
-        for x, y in self.d:
-            sqdakt = (x - p[0]) ** 2 + (y - p[1]) ** 2
-            sqd.append(sqdakt)
-        idx = numpy.argsort(sqd)
-        i = 0
-        for x in idx[1:]:
-            if sqd[x] < eps ** 2:
-                neighbors.append(self.d[x])
-
-        neighbors = numpy.array(neighbors)
-        return neighbors
-
-
     def nnPca(self, p, eps):
         """
         gibt die Nachbarn einer Punktes P in der Umgebung eps aus bricht fruehzeitig ab wenn die bedingung erfuellt ist
